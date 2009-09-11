6d4f07cffe5a8e2fe84b651a1bb6c81c40ec06a7
Cameron Stone <cameronsstone@gmail.com>
2009-09-11 19:14:21 +1000

    sound_capture now builds with the Mk system. Have not tested yet

---
+++ s/sound_capture/i.h
@@ -0,0 +1,31 @@
+#ifndef SOUND_CAPTURE_H
+#define SOUND_CAPTURE_H
+#include <stdbool.h>
+#include <float.h>
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
+typedef enum action {
+	A_SET,
+	A_GET,
+	A_ENABLE,
+	A_DISABLE,
+	A_RESET,
+	A_SET_INPUT,
+	A_SET_OUTPUT,
+	A_DIRECTION,
+	A_TEST,
+} action_t;
+
+
+#include "bowerbird.h"
+#include "sound_capture-prototypes.h"
+
+#endif
