de85409416826bf22b04f990cdd55fd490efcc28
Andrew Taylor <andrewt@cse.unsw.edu.au>
2011-11-29 12:00:16 +1100

    sound_capture format

--- s/sound_capture/sound_capture.c
+++ s/sound_capture/sound_capture.c
@@ -3,7 +3,7 @@
 #define SOUND_CAPTURE_GROUP "sound_capture"
 #define VERSION "0.1.1"
 #define FILE_DIR_FORMAT "%d_%02d_%02d" // args are year, month, day
-#define FILE_NAME_FORMAT "%s/%s/%02d_%02d_%02d_%02d.%s" // args are data_dir, file_dir(date), hours, minutes, seconds, microseconds, file extension
+#define FILE_NAME_FORMAT "%s/%s/%02d_%02d_%02d_%06d.%s" // args are data_dir, file_dir(date), hours, minutes, seconds, microseconds, file extension
 
 #define PARAMETER_PARSING_VERBOSITY 20 // verbosity for parsing cmdline & file parameters
 #define PROGRAM_VERBOSITY 30
